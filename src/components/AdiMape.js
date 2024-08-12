10d416d3af14afb96497b5efa47a24dce763fb0c
Vincent Lewi <vincentlewii@gmail.com>
2024-08-12 16:54:33 +0800

    chore: remove console log

--- src/components/AdiMape.js
+++ src/components/AdiMape.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 const AdiMape = () => {
-  console.log('AdiMape');
   return (
     <div className='App'>
-      1
       <iframe 
         src='mape_map.html'
         style={{ width: '100%', height: '100%', border: 'none' }}
